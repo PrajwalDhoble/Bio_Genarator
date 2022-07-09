357485478cbafa052528cc3753b5a3a04eaf94ee
PrajwalDhoble <prsjwaldhoble2000@gmail.com>
2022-07-09 16:24:28 +0530

    google translate added

--- src/Components/Header/BioPage.jsx
+++ src/Components/Header/BioPage.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import "./BioPage.css";
-
+import axios from "axios"
 function BioPage() {
+  const [options, setOptions] = useState([]);
+  const [to, setTo] = useState('en');
+  const [from, setFrom] = useState('en');
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
   const [data, setdata] = useState({
     name: "",
     gender: "male",
@@ -12,14 +17,48 @@ function BioPage() {
     religious: "",
     reason: "",
   });
-
-
   const [location, setlocation] = useState(!false)
   const [school, setSchool] = useState(!false)
   const [major, setMajor] = useState(!false)
   const [occupation, setOccupation] = useState(!false)
   const [religion, setReligion] = useState(!false)
   const [reason, setReason] = useState(!false)
+
+
+
+  const translate = () => {
+    // curl -X POST "https://libretranslate.de/translate" -H  "accept: application/json" -H  "Content-Type: application/x-www-form-urlencoded" -d "q=hello&source=en&target=es&api_key=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+   
+    const params = new URLSearchParams();
+    params.append('q', input);
+    params.append('source', from);
+    params.append('target', to);
+    params.append('api_key', 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
+
+    axios.post('https://libretranslate.de/translate',params, {
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    }).then(res=>{
+      console.log(res.data)
+      setOutput(res.data.translatedText)
+    })
+  };
+
+  useEffect(() => {
+    axios
+      .get('https://libretranslate.de/languages', {
+        headers: { accept: 'application/json' },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setOptions(res.data);
+      });
+  }, []);
+
+  //  curl -X GET "https://libretranslate.de/languages" -H  "accept: application/json"
+
 
 
 
@@ -262,9 +301,50 @@ function BioPage() {
               {data.name} is from {data.location}.{(data.gender) == 'male' ? 'He' : 'She'} is studying {data.major} at {data.school}.{(data.gender) == 'male' ? 'He' : 'She'} currently works as a {data.occupation}.{(data.gender) == 'male' ? 'He' : 'She'} was raised {data.religious}.{(data.gender) == 'male' ? 'He' : 'She'} {data.reason}.
             </p>
           </div>
-        </div>
-
+          {/* </div> */}
+          <br></br>
+          <div>
+       <br></br>
       </div>
+        </div>
+        <div>
+        <div>
+          <b>Copy and paste your bio here and translate in which language you want</b>
+          <br></br>
+        From ({from}) :
+        <select onChange={(e) => setFrom(e.target.value)}>
+          {options.map((opt) => (
+            <option key={opt.code} value={opt.code}>
+              {opt.name}
+            </option>
+          ))}
+        </select>
+        To ({to}) :
+        <select onChange={(e) => setTo(e.target.value)}>
+          {options.map((opt) => (
+            <option key={opt.code} value={opt.code}>
+              {opt.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        
+        <textarea
+          cols="50"
+          rows="8"
+          onInput={(e) => setInput(e.target.value)}
+        > </textarea>
+      </div>
+      <div>
+        <textarea cols="50" rows="8" value={output}></textarea>
+      </div>
+      <div>
+        <button onClick={e=>translate()}>Translate</button>
+      </div>
+      {/* </div> */}
+        </div>
+       </div>
 
     </>
 
